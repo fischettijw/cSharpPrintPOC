c6375442487d7fd2641fd6ff6d12e15855b7ab8e
Joseph Fischetti <fischettijw@gmail.com>
2020-05-23 16:39:41 -0400

    Add project files.

---
+++ FrmTesting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpPrintPOC
+{
+    public partial class FrmTesting : Form
+    {
+        ListBoxPrint p1;
+        ListBoxPrint p2;
+        public FrmTesting()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTesting_Load(object sender, EventArgs e)
+        {
+            LbxOutput.Items.Clear();
+            p1 = new ListBoxPrint(LbxOutput, 30, 8, 15); //Set Column Widths
+            p1.Print("Col #1", "Col #2", "Col #3");  //Print Header Line
+            p1.Print();
+
+            p2 = new ListBoxPrint(LbxOutput, 15, 10, 25); //Set Column Widths
+            p2.Print("Col #4", "Col #5", "Col #6");  //Print Header Line
+            p2.Print();
+        }
+
+        private void BtnClick_Click(object sender, EventArgs e)
+        {
+            p1.Print("n=: "+"123", 456, 789.12345);   //Print Data Row
+            p2.Print("456", 123, 987.12345);   //Print Data Row
+        }
+
+    }
+
+}
---
+++ ListBoxPrint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpPrintPOC
+{
+    public class ListBoxPrint
+    {
+        public ListBox Lbx { get; set; }
+        public int W1 { get; set; }
+        public int W2 { get; set; }
+        public int W3 { get; set; }
+        public int W4 { get; set; }
+
+        public ListBoxPrint(ListBox lbx, int w1 = 0, int w2 = 0, int w3 = 0, int w4 = 0)
+        {
+            Lbx = lbx;
+            W1 = w1;
+            W2 = w2;
+            W3 = w3;
+            W4 = w4;
+        }
+
+        public void Print(object V1 = null, object V2 = null, object V3 = null, object V4 = null)
+        {
+            string output = "";
+            string v1 = ""; string v2 = ""; string v3 = ""; string v4 = "";
+            if (V1 != null) v1 = V1.ToString().RJ(W1);
+            if (V2 != null) v2 = V2.ToString().RJ(W2);
+            if (V3 != null) v3 = V3.ToString().RJ(W3);
+            if (V4 != null) v4 = V4.ToString().RJ(W4);
+            output = v1 + v2 + v3 + v4;
+            Lbx.Items.Add(output);
+        }
+
+
+
+    }
+}
